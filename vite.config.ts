2e3cb079c1f83aead68190e50908e49696f19cba
shiang1210 <hcs1210@gmail.com>
2026-04-03 14:48:40 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
-  // 針對 GitHub Pages 專案網站，設定為儲存庫名稱
   base: '/smartboard/'
 });
