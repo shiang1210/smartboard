d0b1f684a2fc6e6fe7587fcb0f3ee1db12d36bad
shiang1210 <hcs1210@gmail.com>
2026-04-03 15:16:27 +0800

    Fix CSS loading issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   base: '/smartboard/'
 });
