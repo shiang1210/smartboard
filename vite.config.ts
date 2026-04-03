acebd0734316db787c4fd3be6593b735a1f64977
shiang1210 <hcs1210@gmail.com>
2026-04-03 15:39:42 +0800

    Fix CSS build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  // 必須對齊 GitHub 儲存庫名稱
+  base: '/smartboard/',
+  build: {
+    // 確保 CSS 不會被拆分過細，利於載入
+    cssCodeSplit: false,
+    assetsDir: 'assets',
+  }
 });
